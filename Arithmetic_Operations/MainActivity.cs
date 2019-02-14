5028db6196fff0a930238a7b6e5bda0296b6b4c8
visualstudio <visualstudio@122-PC18>
2019-02-14 10:50:48 -0500

    new app created

---
+++ Arithmetic_Operations/MainActivity.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Arithmetic_Operations
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText input1, input2, op;
+        Button plus, minus, product, divide;
+        
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            input1 = FindViewById<EditText>(Resource.Id.editText1);
+            input2 = FindViewById<EditText>(Resource.Id.editText2);
+            op = FindViewById<EditText>(Resource.Id.editText3);
+            plus = FindViewById<Button>(Resource.Id.button1);
+            minus = FindViewById<Button>(Resource.Id.button2);
+            product = FindViewById<Button>(Resource.Id.button3);
+            divide = FindViewById<Button>(Resource.Id.button4);
+            
+            
+                plus.Click += delegate
+                {
+                    if (input1.Text != "" && input2.Text != "")
+                    {
+                        double a = Convert.ToDouble(input1.Text);
+                        double b = Convert.ToDouble(input2.Text);
+                        op.Text = input1.Text + " + " + input2.Text + " = " + Convert.ToString((Convert.ToDouble(input1.Text)) + (Convert.ToDouble(input2.Text)));
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Enter values first", ToastLength.Long).Show();
+                        if (op.Text == "")
+                        {
+
+                        }
+                        else if (op.Text != "")
+                        {
+                            op.Text = "";
+                        }
+                    }
+
+                };
+
+            minus.Click += delegate
+            {
+                if (input1.Text != "" && input2.Text != "")
+                {
+                    double a = Convert.ToDouble(input1.Text);
+                    double b = Convert.ToDouble(input2.Text);
+                    op.Text = input1.Text + " - " + input2.Text + " = " + Convert.ToString((Convert.ToDouble(input1.Text)) - (Convert.ToDouble(input2.Text)));
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter values first", ToastLength.Long).Show();
+                    if (op.Text == "")
+                    {
+
+                    }
+                    else if (op.Text != "")
+                    {
+                        op.Text = "";
+                    }
+                }
+
+            };
+
+            product.Click += delegate
+            {
+                if (input1.Text != "" && input2.Text != "")
+                {
+                    double a = Convert.ToDouble(input1.Text);
+                    double b = Convert.ToDouble(input2.Text);
+                    op.Text = input1.Text + " * " + input2.Text + " = " + Convert.ToString((Convert.ToDouble(input1.Text)) * (Convert.ToDouble(input2.Text)));
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter values first", ToastLength.Long).Show();
+                    if (op.Text == "")
+                    {
+
+                    }
+                    else if (op.Text != "")
+                    {
+                        op.Text = "";
+                    }
+                }
+
+            };
+
+            divide.Click += delegate
+            {
+                if (input1.Text != "" && input2.Text != "")
+                {
+                    double a = Convert.ToDouble(input1.Text);
+                    double b = Convert.ToDouble(input2.Text);
+                    op.Text = input1.Text+" / "+input2.Text+" = "+Convert.ToString((Convert.ToDouble(input1.Text)) / (Convert.ToDouble(input2.Text)));
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enter values first", ToastLength.Long).Show();
+                    if (op.Text == "")
+                    {
+
+                    }
+                    else if (op.Text != "")
+                    {
+                        op.Text = "";
+                    }
+                }
+
+            };
+
+
+            //minus.Click += delegate
+            //{
+            //    op.Text = Convert.ToString(Convert.ToDouble(input1.Text) - Convert.ToDouble(input2.Text));
+            //};
+            //product.Click += delegate
+            //{
+            //    op.Text = Convert.ToString(Convert.ToDouble(input1.Text) * Convert.ToDouble(input2.Text));
+            //};
+            //divide.Click += delegate
+            //{
+            //    op.Text = Convert.ToString(Convert.ToDouble(input1.Text) / Convert.ToDouble(input2.Text));
+            //};
+
+
+        }
+
+        
+
+       
+
+        
+	}
+}
+
